626d4b79e1de28ee9ce62444962a875af1e56f7d
myunusyesil <mehmetyunus@windowslive.com>
2022-09-14 15:01:49 +0300

    input form added

--- app.js
+++ app.js
@@ -2,9 +2,66 @@ const container = document.querySelector(".tbody")
 const initBtn = document.querySelector(".btn-primary")
 const clearBtn = document.querySelector(".btn-clear")
 
+// form elements
+const inputName = document.querySelector(".name");
+const inputUsername = document.querySelector(".username");
+const inputCompanyName = document.querySelector(".company-name");
+const inputPhone = document.querySelector(".phone");
+const inputEmail = document.querySelector(".email");
+const form = document.querySelector("form");
+console.log(form);
+
+var counter = 1;
+
+const user = { 
+    name: "",
+    username: "",
+    companyName: "",
+    phone: 05321234567,
+    email: ""
+} 
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    user.name = inputName.value;
+    user.username = inputUsername.value;
+    user.companyName = inputCompanyName.value;
+    user.phone = inputPhone.value;
+    user.email = inputPhone.value;
+    
+    initFormUser(user);
+    loadDefault();
+})
+
+console.log(user);
+
+
 initBtn.addEventListener('click', initTable);
 clearBtn.addEventListener('click', clearTable);
 
+function loadDefault () {
+    inputName.value = "";
+    inputUsername.value = "";
+    inputCompanyName.value = "";
+    inputPhone.value = "";
+    inputEmail.value = "";
+}
+
+function initFormUser (userSubmit) {
+    let rowInput = `
+    <tr class="each-row">
+    <th scope="row">${counter}</th>
+    <td>${userSubmit.name}</td>
+    <td>${userSubmit.username}</td>
+    <td>${userSubmit.companyName}</td>
+    <td>${userSubmit.phone}</td>
+    <td>${userSubmit.email}</td>
+    <th scope="col"> <button class="btn btn-danger">Delete</button></th>
+    </tr>
+`   
+    container.innerHTML += rowInput;
+         
+}
 
 // console.log(container)
 
@@ -29,6 +86,8 @@ function initTable () {
                 `
                 // console.log(row);
                 container.innerHTML += row;
+                counter = element.id;
+                counter++;
             });
             const deleteBtns = document.querySelectorAll(".btn-danger")
             console.log(deleteBtns)
@@ -45,5 +104,3 @@ function initTable () {
 function clearTable () {
     container.innerHTML = "";
 }
-
-
